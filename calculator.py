c92f15b7f6306be996f14656c70b8bdc7d8c4bf1
meghajadav <meghasaraiya@gmail.com>
2025-09-21 22:12:27 +0530

    updated code

--- calculator.py
+++ calculator.py
@@ -2,10 +2,14 @@ from mcp.server.fastmcp import FastMCP
 
 mcp =FastMCP('math')
 
-@mcp.tool
+@mcp.tool()
 def add(a:int, b:int)->int:
     return a+b
 
-@mcp.tool
+@mcp.tool()
 def multiply(a:int, b:int)->int:
     return a*b
+
+
+if __name__=='__main__':
+    mcp.run(transport='stdio')
--- weather.py
+++ weather.py
@@ -2,6 +2,10 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP('weather')
 
-@mcp.tool
+@mcp.tool()
 async def get_weather(location:str)->str:
     return 'New York weather is pleasant.'
+
+
+if __name__=='__main__':
+    mcp.run(transport='streamable-http')
